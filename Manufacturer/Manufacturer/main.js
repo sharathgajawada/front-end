e1c86c96e3e4f6a574f8a1c38883499810cea049
sharathgajawada <80203491+sharathgajawada@users.noreply.github.com>
2024-01-20 12:21:02 +0530

    Add files via upload

---
+++ Manufacturer/Manufacturer/main.js
@@ -0,0 +1,15 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+navToggle.addEventListener('click', () =>{
+   navMenu.classList.add('show-menu')
+})
+
+/* Menu hidden */
+navClose.addEventListener('click', () =>{
+   navMenu.classList.remove('show-menu')
+})
+
